94d93026c5f6cad18ace60b9e253aa73f2fdc821
anveretal <anveretal@mail.ru>
2025-12-19 22:00:02 +0700

    added pointer_array.c and pointer_array.h

---
+++ pointer_array.c
@@ -0,0 +1,61 @@
+#include <stdlib.h>
+
+#include "pointer_array.h"
+
+pointerArray_t *pointerArrayCreate(size_t capacity) {
+    pointerArray_t *pointerArray = malloc(sizeof(*pointerArray));
+    if (!pointerArray) return NULL;
+
+    pointerArray->data = calloc(capacity, sizeof(void *));
+    if (!pointerArray->data) {
+        free(pointerArray);
+        return NULL;
+    }
+
+    pointerArray->size = 0;
+    pointerArray->capacity = capacity;
+
+    return pointerArray;
+}
+
+void pointerArrayDestroy(pointerArray_t *pointerArray, void (*destroyFunction)(void *)) {
+    if (!pointerArray) return;
+
+    for (size_t i = 0; i < pointerArray->size; i++) {
+        void *currentItem = pointerArray->data[i];
+        if (currentItem) {
+            if (destroyFunction) destroyFunction(currentItem);
+            else free(currentItem);
+        }
+    }
+
+    free(pointerArray->data);
+    free(pointerArray);
+}
+
+
+
+int pointerArrayAdd(pointerArray_t *pointerArray, void *item) {
+    if (!pointerArray) return EXIT_FAILURE;
+
+    if (pointerArray->size >= pointerArray->capacity) {
+        size_t newCapacity = pointerArray->capacity ? pointerArray->capacity * 2 : 1;
+
+        pointerArray->data = realloc(pointerArray->data, sizeof(void *) * newCapacity);
+        if (!pointerArray->data) return EXIT_FAILURE;
+
+        pointerArray->capacity = newCapacity;
+    }
+
+    pointerArray->data[pointerArray->size] = item;
+    pointerArray->size++;
+
+    return EXIT_SUCCESS;
+}
+
+void *pointerArrayGetAt(pointerArray_t *pointerArray, size_t index) {
+    if (!pointerArray || index >= pointerArray->size)
+        return NULL;
+
+    return pointerArray->data[index];
+}
---
+++ pointer_array.h
@@ -0,0 +1,19 @@
+#ifndef POINTER_ARRAY_H
+#define POINTER_ARRAY_H
+
+#include <stddef.h>
+
+typedef struct {
+    size_t size;
+    size_t capacity;
+    void **data;
+} pointerArray_t;
+
+pointerArray_t *pointerArrayCreate(size_t capacity);
+void pointerArrayDestroy(pointerArray_t *pointerArray, void (*destroyFunction)(void *));
+
+int pointerArrayAdd(pointerArray_t *pointerArray, void *item);
+void *pointerArrayGetAt(pointerArray_t *pointerArray, size_t index);
+
+
+#endif // POINTER_ARRAY_H
